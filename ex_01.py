3f4f42e45d8520c56221210d8f4778deaf749f37
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-04-08 20:44:59 -0300

    salvando

---
+++ ex_01.py
@@ -0,0 +1,20 @@
+nome_produto: str = "Mesa de Escritório"
+quantidade_estoque: int = 10
+preco_unitario: float = 450.0
+disponivel_venda: bool = True
+data_validade = 2024-12-31
+
+# print("Ola mundo!")
+# print(quantidade_estoque)
+# print(f"meu produto é o: {nome_produto}")
+
+# valor_inserido_pelo_usuario: str = input("Digite um texto aqui: ")
+# print(valor_inserido_pelo_usuario)
+
+print(type(nome_produto))
+print(nome_produto)
+
+nome_produto_maiusculo = nome_produto.upper()
+print(nome_produto_maiusculo)
+
+quantidade_estoque
